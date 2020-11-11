0c9aeebf06166db3687bad7442388ece4f598c05
1848116862@qq.com <1848116862@qq.com>
2020-11-11 21:24:41 +0800

    2020_11_11

---
+++ test/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ll/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ll/szzq_ws/devel;/home/ll/dobot_ws/devel;/home/ll/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/ll/szzq_ws/devel;/home/ll/dobot_ws/devel;/home/ll/catkin_ws/devel;/opt/ros/melodic' != '' else []
